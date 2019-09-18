41090ca58de44bbeaad262d9a68ae511660edcca
Zakladnyi Oleg <zakladniy@gmail.com>
2019-09-18 18:05:08 +0300

    correct accordind to ESLINT

--- 1homework.js
+++ 1homework.js
@@ -1,6 +1,7 @@
+'use strict';
 //задание № 1
 //идентификаторы
-let name = "Oleg";
+const name = 'Oleg';
 const birth = 1982;
 const hy = name => console.log(`Привет,  ${name}`);
 hy('Petro');
@@ -9,110 +10,101 @@ hy(birth);
 
 //циклы
 
-for (let i = 15; i <= 30; i+=2) console.log(i);
+for (let i = 15; i <= 30; i += 2) console.log(i);
 
-const range = (start, end ) => 
-{
-   while (start <= end)  
-    {
-    if (start % 2 !== 0 )  console.log(start);
-     
-       ++start;  
-    }   
-}
+const range = (start, end) => {
+  while (start <= end) {
+    if (start % 2 !== 0) console.log(start);
+    ++start;
+  }
+};
 range(15, 30);
 
 //функции
-const average = (a, b) => {
-return (a/2+b/2);
-}
+const average = (a, b) => (a / 2 + b / 2);
 console.log(average(7, 14));
 
-const square = (a) => {
-return a**2;    
-}
+const square = a => a ** 2;
 console.log(square(5));
 
-const qube = (a) => {
-return a**3;
-}
+const qube = a => a ** 3;
 
-for (let i = 1; i<=5; i++) 
-console.log(average(square(i), qube(i)));
+
+for (let i = 1; i <= 5; i++)
+  console.log(average(square(i), qube(i)));
 
 //Обекты
-{const name = "Oleg";
-let fio = {name};
-let zio = {name};
-console.log(fio);
-console.log(zio);
-let labla = "Petro";
-fio = {labla};
-zio = {labla};
-console.log(fio);
-console.log(zio);
-const fish = "karp";
-labla = fish;
-console.log(fio);
-console.log(zio);
+{
+  const name = 'Oleg';
+  let fio = { name };
+  let zio = { name };
+  console.log(fio);
+  console.log(zio);
+  let labla = 'Petro';
+  fio = { labla };
+  zio = { labla };
+  console.log(fio);
+  console.log(zio);
+  const fish = 'karp';
+  labla = fish;
+  console.log(fio);
+  console.log(zio);
 
-function createUser (name, city) {
-console.log({name: name, city: city})
-};
-createUser('Marcus Aurelius', 'Roma');
+  function createUser(name, city) {
+    console.log({ name, city });
+  }
+  createUser('Marcus Aurelius', 'Roma');
 }
 //Массивы
- let addressBook = [
-     {
-         name: 'Marcus Aurelius',
-         phone: '+380445554433'
-     },
-     {
-        name: 'Oleg The King',
-        phone: '+380123456789'
-     }
- ];
+const addressBook = [
+  {
+    name: 'Marcus Aurelius',
+    phone: '+380445554433'
+  },
+  {
+    name: 'Oleg The King',
+    phone: '+380123456789'
+  }
+];
 
- function findPhoneByName(name) {
-     for (let i = 0; i < addressBook.length; ++i) {
-         if (addressBook[i].name === name) 
-         console.log(addressBook[i].phone);
-     }
- };
- findPhoneByName('Marcus Aurelius');
-//перебор массива forEach
- function findPhoneByName(name) {
- addressBook.forEach(function(element) {
-         
-        if (element.name === name) 
-        console.log(element.phone);
-    }
- )
-};
-findPhoneByName('Marcus Aurelius');
-
-//перебор массива for in
 function findPhoneByName(name) {
-   for (let value of addressBook) {
-            
-           if (value.name === name) 
-           console.log(value.phone);
-       }
-    
-   };
-   findPhoneByName('Marcus Aurelius');
-
- //хеш таблицы
- {
- let addressBook = {
-        name: 'Marcus Aurelius',
-        phone: '+380445554433'
-};
-function findPhoneByName(name) {
-    for(let key in addressBook) {
-        if (addressBook[key] === name) 
-        console.log(addressBook.phone);
-    }
+  for (let i = 0; i < addressBook.length; ++i) {
+    if (addressBook[i].name === name)
+      console.log(addressBook[i].phone);
+  }
 }
 findPhoneByName('Marcus Aurelius');
- }
+//перебор массива forEach
+function findPhoneByName2(name) {
+  addressBook.forEach(element => {
+    if (element.name === name) console.log(element.phone);
+  }
+  );
+}
+findPhoneByName2('Marcus Aurelius');
+
+//перебор массива for in
+function findPhoneByName1(name) {
+  for (const value of addressBook) {
+
+    if (value.name === name)
+      console.log(value.phone);
+  }
+
+}
+findPhoneByName1('Marcus Aurelius');
+
+//хеш таблицы
+{
+  const addressBook = {
+    name: 'Marcus Aurelius',
+    phone: '+380445554433'
+  };
+  function findPhoneByName(name) {
+    for (const key in addressBook) {
+      if (addressBook[key] === name)
+        console.log(addressBook.phone);
+    }
+  }
+  findPhoneByName('Marcus Aurelius');
+}
--- 2homework.js
+++ 2homework.js
@@ -1,34 +1,35 @@
+'use strict';
 // задание №2
 // поднятие
 console.log(host());
-function host () {
-    let a = 3;
-    return a
+function host() {
+  const a = 3;
+  return a;
 }
 //скалярные и сылочные типы данных
 const a = 5;
-const inc = numbr => numbr*2;
+const inc = numbr => numbr * 2;
 const b = inc(a);
-console.log({a, b});
-
-const inc = num => num.n+=3;
-const obj = {n: 5};
-inc(obj);
-console.log (obj);
-
+console.log({ a, b });
+{
+  const inc = num => num.n += 3;
+  const obj = { n: 5 };
+  inc(obj);
+  console.log(obj);
+}
 // типы обектов
 
 const mas = [true, 'hello', 5, 12, -200, false, false, 'word', 'kiev', 145];
 const col = { number: 0, string: 0, boolean: 0 };
 
 const calc = arr => {
-for ( let element of arr) {
-   typeof element === 'number' ?
-   col.number++ :
-   typeof element === "string"  ?
-   col.string++  :
-   col.boolean++;
-}
-}
+  for (const element of arr) {
+    typeof element === 'number' ?
+      col.number++ :
+      typeof element === 'string'  ?
+        col.string++  :
+        col.boolean++;
+  }
+};
 calc(mas);
 console.log(col);
